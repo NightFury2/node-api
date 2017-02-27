c83549a51342273b70014f8544b8c35da5fe32d1
nightfury <lunvvova42@gmail.com>
2017-02-27 10:54:15 +0300

    fix ReadMe list

--- bin/config.js
+++ bin/config.js
@@ -13,25 +13,8 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.PORT,
   app: {
-    title: 'ReactTestReq',
-    description: 'ReactTestReq',
-    head: {
-      titleTemplate: 'ReactTestReq: %s',
-      meta: [
-        {name: 'description', content: 'ReactTestReq'},
-        {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'ReactTestReq'},
-        {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
-        {property: 'og:locale', content: 'ru_RU'},
-        {property: 'og:title', content: 'ReactTestReq'},
-        {property: 'og:description', content: 'ReactTestReq'},
-        {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@nightfury2'},
-        {property: 'og:creator', content: '@nightfury2'},
-        {property: 'og:image:width', content: '200'},
-        {property: 'og:image:height', content: '200'}
-      ]
-    }
+    title: 'Node-api',
+    description: 'Test Api from React application'
   },
 
 }, environment);
